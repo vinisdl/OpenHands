a6ead0c9953af5668cc59d54977563d693195293
Marco Dall'Alba <marco.dalalba@dbserver.com.br>
2025-11-09 17:21:29 -0300

    adding zaure pipeline

--- containers/app/config.sh
+++ containers/app/config.sh
@@ -1,4 +1,8 @@
-DOCKER_REGISTRY=ghcr.io
+# Usar variável de ambiente DOCKER_REGISTRY se definida, caso contrário usar ghcr.io
+# Não sobrescrever se já estiver definida como variável de ambiente
+if [[ -z "${DOCKER_REGISTRY:-}" ]]; then
+  DOCKER_REGISTRY=ghcr.io
+fi
 DOCKER_ORG=all-hands-ai
 DOCKER_IMAGE=openhands
 DOCKER_BASE_DIR="."
