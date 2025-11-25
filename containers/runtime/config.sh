4e42c211a51322203236a4f4603af6507d1fc2ec
Marco Dall'Alba <marco.dalalba@dbserver.com.br>
2025-11-25 13:41:55 -0300

    pipe

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -1,5 +1,9 @@
-DOCKER_REGISTRY=ghcr.io
-DOCKER_ORG=openhands
+# Usar variável de ambiente DOCKER_REGISTRY se definida, caso contrário usar ghcr.io
+# Não sobrescrever se já estiver definida como variável de ambiente
+if [[ -z "${DOCKER_REGISTRY:-}" ]]; then
+  DOCKER_REGISTRY=ghcr.io
+fi
+DOCKER_ORG=all-hands-ai
 DOCKER_BASE_DIR="./containers/runtime"
 DOCKER_IMAGE=runtime
 # These variables will be appended by the runtime_build.py script
