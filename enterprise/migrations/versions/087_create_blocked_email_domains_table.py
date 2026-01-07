5c20724845d0bdee17a113b0bbadae958f297b43
Hiep Le <69354317+hieptl@users.noreply.github.com>
2026-01-07 17:29:31 +0700

    fix: db migration (#12282)

--- enterprise/migrations/versions/087_create_blocked_email_domains_table.py
+++ enterprise/migrations/versions/087_create_blocked_email_domains_table.py
@@ -1,7 +1,7 @@
 """create blocked_email_domains table
 
-Revision ID: 086
-Revises: 085
+Revision ID: 087
+Revises: 086
 Create Date: 2025-01-27 00:00:00.000000
 
 """
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '086'
-down_revision: Union[str, None] = '085'
+revision: str = '087'
+down_revision: Union[str, None] = '086'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
